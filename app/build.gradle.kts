6e570705200d59963e03e7a1e487c61e0954bf57
vicezapata <vice.zapata@profesor.duoc.cl>
2025-10-11 09:55:38 -0300

    Formulario

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,11 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
 
+// Íconos (core opcional) y EXTENDIDOS (¡este es el clave!)
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+
+
 
 
 
