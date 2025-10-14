74d64436f407b2974d224686bcf38b304f52771f
vicezapata <vice.zapata@profesor.duoc.cl>
2025-10-13 22:08:30 -0300

    Primera version persistencia

--- app/src/main/java/com/example/login002v/navigation/AppNav.kt
+++ app/src/main/java/com/example/login002v/navigation/AppNav.kt
@@ -1,5 +1,6 @@
 package com.example.login002v.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavType
@@ -9,6 +10,8 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.login002v.ui.home.MuestraDatosScreen
 import com.example.login002v.ui.login.LoginScreen
+import com.example.login002v.view.DrawerMenu
+import com.example.login002v.view.ProductoFormScreen
 
 @Composable
 
@@ -25,23 +28,40 @@ fun AppNav(){
         }//fin composable
 
         composable(
-            route="muestraDatos/{username}",
+            //route="muestraDatos/{username}",
+            route="DrawerMenu/{username}",
             arguments = listOf(
                 navArgument("username"){
                     type= NavType.StringType
                 }
             )//fin List Of
-        )//fin composable
+        )//fin composable 2
 
         { // inicio
             backStackEntry ->
             val username = backStackEntry.arguments?.getString("username").orEmpty()
-            MuestraDatosScreen(username=username,navController= navController )
+           // MuestraDatosScreen(username=username,navController= navController )
+            DrawerMenu(username=username,navController= navController )
 
         }
 
+///  Ruta del Formulario:  ProductoFormScreen
 
+        composable(
+            route="ProductoFormScreen/{nombre}/{precio}",
+            arguments = listOf(
+                navArgument("nombre"){ type= NavType.StringType },
+                navArgument("precio"){ type= NavType.StringType },
+            )//fin List Of
+        ) // fin composable 3
 
+        { // inicio
+                backStackEntry ->
+            val nombre = Uri.decode(backStackEntry.arguments?.getString("nombre") ?:"")
+            val precio = backStackEntry.arguments?.getString("precio") ?:""
+
+            ProductoFormScreen( navController= navController,  nombre=nombre,precio= precio )
+        }
 
     }// Fin NavHost
 
